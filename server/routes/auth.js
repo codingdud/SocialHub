8e2433d26f0bbd1e03f6012b4815f1e29efac239
codingdud <animesh384@gmail.com>
2024-05-07 21:31:48 +0530

    router update

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,9 @@ import express from "express";
 import { login } from "../controllers/auth.js";
 
 const router = express.Router();
-
+router.get("/", (req, res) => {
+  res.send("i am ok");
+});
 router.post("/login", login);
 
 export default router;
